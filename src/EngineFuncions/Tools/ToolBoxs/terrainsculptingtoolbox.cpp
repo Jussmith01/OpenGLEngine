6ccb35b10eb250539c1caa8a3714d5d74794dd89
Justin Smith <jussmith48@gmail.com>
2015-06-22 20:16:55 -0400

    Setup Terrain Sculpting Toolbox

--- src/EngineFuncions/Tools/ToolBoxs/terrainsculptingtoolbox.cpp
+++ src/EngineFuncions/Tools/ToolBoxs/terrainsculptingtoolbox.cpp
@@ -1,4 +1,4 @@
-#include "terrainmodificationtoolbox.h"
+#include "terrainsculptingtoolbox.h"
 
 //************************************
 // Drop Box Initilization Function
@@ -9,7 +9,7 @@ Initialize the menu dropbox box class.
 PARAMETERS:
 
 */
-void TerrainModificationToolbox::Init(float posx, float posy,Properties *props,ISoundEngine* audioengine,TerrainModificationData &tmdata)
+void TerrainSculptingToolbox::Init(float posx, float posy,Properties *props,ISoundEngine* audioengine)
 {
     this->props=props;
     this->audioengine=audioengine;
@@ -23,27 +23,6 @@ void TerrainModificationToolbox::Init(float posx, float posy,Properties *props,I
     // Setup the frame
     frame.Init(0.3f,0.2f,0.01f,*props);
     frame.SetColors(glm::vec4(0.6f,0.6f,0.6f,0.5f),glm::vec4(0.2f,0.2f,0.2f,1.0f));
-
-    // Setup the insertion boxes
-    insertbox[0].Init(x+0.05,y+0.15,0.04f,"solid",*props,audioengine,0);
-    std::stringstream ss1;
-    ss1 << tmdata.heightMult;
-    insertbox[0].SetData(ss1.str());
-
-    insertbox[1].Init(x+0.05,y+0.05,0.04f,"solid",*props,audioengine,0);
-    std::stringstream ss2;
-    ss2 << tmdata.relheight[0];
-    insertbox[1].SetData(ss2.str());
-
-    insertbox[2].Init(x+0.05,y-0.05,0.04f,"solid",*props,audioengine,0);
-    std::stringstream ss3;
-    ss3 << tmdata.relheight[1];
-    insertbox[2].SetData(ss3.str());
-
-    insertbox[3].Init(x+0.05,y-0.15,0.04f,"solid",*props,audioengine,0);
-    std::stringstream ss4;
-    ss4 << tmdata.relheight[2];
-    insertbox[3].SetData(ss4.str());
 
     // Setup the screen writer class
     text.Setup("../Fonts/FreeSans.ttf",props->WinWidth,props->WinHeight,props->FontSize);
@@ -61,20 +40,10 @@ void TerrainModificationToolbox::Init(float posx, float posy,Properties *props,I
 This function carries out all draw calls to draw
 the buttons to the screen.
 */
-void TerrainModificationToolbox::DrawToolBox()
+void TerrainSculptingToolbox::DrawToolBox()
 {
     // Draw
     frame.DrawBoxPos(x,y);
-
-    text.RenderTextLeftJustified("Height Multiplier: ",x-0.27,y+0.17,1.0f,glm::vec3(1.0f));
-    insertbox[0].DrawInsBox();
-    text.RenderTextLeftJustified("Low->Mid Switch (0-1): ",x-0.27,y+0.07,1.0f,glm::vec3(1.0f));
-    insertbox[1].DrawInsBox();
-    text.RenderTextLeftJustified("Mid->High Switch (0-1): ",x-0.27,y-0.03,1.0f,glm::vec3(1.0f));
-    insertbox[2].DrawInsBox();
-    text.RenderTextLeftJustified("Switch Mixing (0-1): ",x-0.27,y-0.13,1.0f,glm::vec3(1.0f));
-    insertbox[3].DrawInsBox();
-
     buttons.DrawButtons();
 };
 
@@ -84,13 +53,9 @@ void TerrainModificationToolbox::DrawToolBox()
 /*
 Cleanup after finished using the buttons
 */
-void TerrainModificationToolbox::Cleanup()
+void TerrainSculptingToolbox::Cleanup()
 {
     frame.Cleanup();
-    insertbox[0].Cleanup();
-    insertbox[1].Cleanup();
-    insertbox[2].Cleanup();
-    insertbox[3].Cleanup();
     text.Cleanup();
     buttons.Cleanup();
 };
@@ -102,16 +67,11 @@ void TerrainModificationToolbox::Cleanup()
 /*
 
 */
-int TerrainModificationToolbox::UpdateEvents(InputStruct &input)
+int TerrainSculptingToolbox::UpdateEvents(InputStruct &input)
 {
     double xmp,ymp;
     input.ReturnMousePos(xmp,ymp);
     bool mpress=input.GetMouseKey(GLFW_MOUSE_BUTTON_LEFT);
-
-    insertbox[0].UpdateEvents(input);
-    insertbox[1].UpdateEvents(input);
-    insertbox[2].UpdateEvents(input);
-    insertbox[3].UpdateEvents(input);
 
     int bpress = buttons.UpdateButtonEvents(input);
 
@@ -147,7 +107,7 @@ This function checks if the mouse, at x,y coords,
 is over the function, then sets that button
 active if it is.
 */
-bool TerrainModificationToolbox::CheckMouseOver(float xmp,float ymp)
+bool TerrainSculptingToolbox::CheckMouseOver(float xmp,float ymp)
 {
     // Check X over
     bool xchk=false;
@@ -176,21 +136,4 @@ bool TerrainModificationToolbox::CheckMouseOver(float xmp,float ymp)
 
     // Return Result
     return Over;
-};
-
-//************************************
-//           Get the Data
-//************************************
-/*
-*/
-TerrainModificationData TerrainModificationToolbox::FetchTerrainModificationData()
-{
-    TerrainModificationData tmdata;
-
-    tmdata.heightMult=insertbox[0].FetchDataString();
-    tmdata.relheight[0]=insertbox[1].FetchDataString();
-    tmdata.relheight[1]=insertbox[2].FetchDataString();
-    tmdata.relheight[2]=insertbox[3].FetchDataString();
-
-    return tmdata;
 };
--- src/EngineFuncions/Tools/ToolBoxs/terrainsculptingtoolbox.h
+++ src/EngineFuncions/Tools/ToolBoxs/terrainsculptingtoolbox.h
@@ -1,5 +1,5 @@
-#ifndef TERRAINMODIFICATIONTOOLBOX_C
-#define TERRAINMODIFICATIONTOOLBOX_C
+#ifndef TERRAINSCULPTINGTOOLBOX_C
+#define TERRAINSCULPTINGTOOLBOX_C
 
 #include "../../../Headers/headerscpp.h"
 #include "../../../Headers/headersogl.h"
@@ -12,58 +12,11 @@
 #include <irrKlang.h>
 #include <ik_ISound.h>
 using namespace irrklang;
-//#pragma comment(lib, "irrKlang.lib") // link with irrKlang.dll
-
-//*****************************************//
-//	      Terrain Creation Data   	       //
-//*****************************************//
-struct TerrainModificationData
-{
-    std::string heightMult;
-    std::string relheight[3];
-
-    TerrainModificationData() {};
-
-    TerrainModificationData(float heightMult,glm::vec3 relheight)
-    {
-        std::stringstream ss[4];
-
-        ss[0] << heightMult;
-        this->heightMult=ss[0].str();
-
-        ss[1] << relheight.x;
-        this->relheight[0]=ss[1].str();
-
-        ss[2] << relheight.y;
-        this->relheight[1]=ss[2].str();
-
-        ss[3] << relheight.z;
-        this->relheight[2]=ss[3].str();
-    };
-
-    //Class Assignment
-    TerrainModificationData& operator=(const TerrainModificationData& instance)
-    {
-        this->heightMult = instance.heightMult;
-        this->relheight[0] = instance.relheight[0];
-        this->relheight[1] = instance.relheight[1];
-        this->relheight[2] = instance.relheight[2];
-        return *this;
-    };
-
-    void ReturnData(float &heightMult,glm::vec3 &relheight)
-    {
-        heightMult=atof(this->heightMult.c_str());
-        relheight.x=atof(this->relheight[0].c_str());
-        relheight.y=atof(this->relheight[1].c_str());
-        relheight.z=atof(this->relheight[2].c_str());
-    };
-};
 
 //*****************************************//
 //	     Terrain Creation Toolbox	       //
 //*****************************************//
-class TerrainModificationToolbox
+class TerrainSculptingToolbox
 {
     // Variables
     std::string sound;
@@ -80,8 +33,7 @@ class TerrainModificationToolbox
     DisplayBox frame;
     ScreenWriter text;
 
-    // Testing!
-    InsertionBox insertbox[4];
+    // Testing
     MenuButtons buttons;
 
     // Check if mouse is over button
@@ -89,7 +41,7 @@ class TerrainModificationToolbox
 
 public:
     // Initialize Button Class
-    void Init(float posx, float posy,Properties* props,ISoundEngine* audioengine,TerrainModificationData &tmdata);
+    void Init(float posx, float posy,Properties* props,ISoundEngine* audioengine);
 
     // Cleanup function for destruction
     void Cleanup();
@@ -99,9 +51,6 @@ public:
 
     // Draw all buttons to the screen
     void DrawToolBox();
-
-    // Draw all buttons to the screen
-    TerrainModificationData FetchTerrainModificationData();
 };
 
 
